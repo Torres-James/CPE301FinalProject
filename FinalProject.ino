374052f8effae347bf67896cb940005954cc0d9b
Torres-James-CS302-1001 <98004523+Torres-James@users.noreply.github.com>
2023-11-16 20:27:31 +0000

    Co-authored-by: 1001-Kokkinis-George <1001-Kokkinis-George@users.noreply.github.com>

---
+++ FinalProject.ino
@@ -0,0 +1,20 @@
+volatile unsigned char* port_k = (unsigned char*)0x108;
+volatile unsigned char* ddr_k = (unsigned char*)0x107;
+volatile unsigned char* pin_k = (unsigned char*)0x106;
+volatile unsigned char* port_e = (unsigned char*)0x2E;
+volatile unsigned char* ddr_e = (unsigned char*)0x2D;
+volatile unsigned char* pin_e = (unsigned char*)0x2C;
+volatile unsigned char* port_d = (unsigned char*)0x2B;
+volatile unsigned char* ddr_d = (unsigned char*)0x2A;
+volatile unsigned char* pin_d = (unsigned char*)0x29;
+volatile unsigned char* port_h = (unsigned char*)0x102;
+volatile unsigned char* ddr_h = (unsigned char*)0x101;
+volatile unsigned char* pin_h = (unsigned char*)0x100;
+
+void setup(){
+
+}
+
+void loop(){
+  
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CPE301FinalProject
 Final Project for CPE 301
+James likes Men
